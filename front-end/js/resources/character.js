c6f5b88bb0e31a12473bd66472b10cff4d73ea97
Fer Martin <fernandomartinsantana@gmail.com>
2015-12-02 10:28:44 +0800

    Added virtual attribute firstName to factory

--- front-end/js/resources/character.js
+++ front-end/js/resources/character.js
@@ -9,7 +9,22 @@ Character.$inject = ['$resource'];
 
 // Factory implementation
 function Character($resource) {
-  return $resource('http://localhost:3000/characters/:id', {id: '@_id'}, {
-    'update': { method:'PUT' }
-  });
+
+  var CharacterResource = $resource(
+    'http://localhost:3000/characters/:id',
+    {id: '@_id'},
+    {'update': { method:'PUT' }}
+  );
+
+  // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+  Object.defineProperty(CharacterResource.prototype, 'firstName', {
+    get: function(){
+      if (this.name) {
+        if (this.name.indexOf(" ") === -1) return this.name;
+        return this.name.slice(0, this.name.indexOf(" "));
+      }
+    }
+  })
+
+  return CharacterResource;
 }
